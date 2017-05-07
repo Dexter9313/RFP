a6b8e9d4e4fede72a3926a3d0655ed22bd9826ce
Florian Cabot <florian.cabot@etu.unige.ch>
2017-05-07 16:03:58 +0200

    Code cleaning

--- src/java/src/ExcelToOnto.java
+++ src/java/src/ExcelToOnto.java
@@ -3,9 +3,10 @@ package ontotest;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class ExcelToOnto {
+public class ExcelToOnto
+{
 	String DocumentCreator;
-	String Creationdate ;
+	String Creationdate;
 	String LastModif;
 	String LastModifC;
 	String Description;
@@ -17,63 +18,71 @@ public class ExcelToOnto {
 	String Template;
 	String Manager;
 	String FileExcelInit;
-	
-	public ExcelToOnto (String FileExcel)
+
+	public ExcelToOnto(String FileExcel)
 	{
-		FileExcelInit=FileExcel;
-		try {
-			ExcelFileExtractor ExcelFile = new ExcelFileExtractor (FileExcelInit);
-			ExcelFile.MetadataExtractor ();
-			DocumentCreator=ExcelFile.DocumentCreator;
-			Creationdate=ExcelFile.Creationdate;
-			LastModif=ExcelFile.LastModif;
-			LastModifC=ExcelFile.LastModifC;
-			 Description=ExcelFile.Description;
-			 Keywords=ExcelFile.Keywords;
-			Title=ExcelFile.Title;
-			Subject=ExcelFile.Subject;
-			Category=ExcelFile.Company;
-			 Company=ExcelFile.Company;
-		     Template=ExcelFile.Template;
-			 Manager=ExcelFile.Manager;
-		} catch (IOException e) {
+		FileExcelInit = FileExcel;
+		try
+		{
+			ExcelFileExtractor ExcelFile
+			    = new ExcelFileExtractor(FileExcelInit);
+			ExcelFile.MetadataExtractor();
+			DocumentCreator = ExcelFile.DocumentCreator;
+			Creationdate    = ExcelFile.Creationdate;
+			LastModif       = ExcelFile.LastModif;
+			LastModifC      = ExcelFile.LastModifC;
+			Description     = ExcelFile.Description;
+			Keywords        = ExcelFile.Keywords;
+			Title           = ExcelFile.Title;
+			Subject         = ExcelFile.Subject;
+			Category        = ExcelFile.Company;
+			Company         = ExcelFile.Company;
+			Template        = ExcelFile.Template;
+			Manager         = ExcelFile.Manager;
+		}
+		catch(IOException e)
+		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	public void ExcelOntoConv( String onto, String FileName)
+
+	public void ExcelOntoConv(String onto, String FileName)
 	{
-		OntoExcel Onto = new OntoExcel (onto);
+		OntoExcel Onto = new OntoExcel(onto);
 		Onto.CreateNewExceClass("Document", "CreatedBy", "Document", "no");
 		Onto.CreateNewExceClass("Auteur", "CreatorOf", "Auteur", "Document");
-		//Onto.CreateNewExceClass("Document", "ModifiedBy", "Document", "Auteur");
+		// Onto.CreateNewExceClass("Document", "ModifiedBy", "Document",
+		// "Auteur");
 		Onto.CreateNewExceClass("Title", "TitleOf", "Title", "Document");
-		Onto.CreateNewExceClass("Description", "DescriptionOf", "Description", "Document");
+		Onto.CreateNewExceClass("Description", "DescriptionOf", "Description",
+		                        "Document");
 		Onto.CreateNewExceClass("Subject", "Concern", "Subject", "Document");
-		Onto.CreateNewExceClass("Category", "hasDocument", "Category", "Document");
-		Onto.CreateNewExceClass("Manager", "ManageSubject", "Manager", "Subject");
+		Onto.CreateNewExceClass("Category", "hasDocument", "Category",
+		                        "Document");
+		Onto.CreateNewExceClass("Manager", "ManageSubject", "Manager",
+		                        "Subject");
 		Onto.CreateNewExceClass("Company", "hasasetof", "Company", "Document");
-		//Onto.CreateNewExceClass("Company", "hasasetof", "Company", "Template");
+		// Onto.CreateNewExceClass("Company", "hasasetof", "Company",
+		// "Template");
 		Onto.CreateNewExceClass("Date", "CreationDateOf", "Date", "Document");
-		
+
 		Onto.CreatNewExcelIndiv("Document", FileExcelInit);
 		Onto.CreatNewExcelIndiv("Auteur", DocumentCreator);
 		Onto.CreatNewExcelIndiv("Title", Title);
 		Onto.CreatNewExcelIndiv("Description", Description);
 		Onto.CreatNewExcelIndiv("Subject", Subject);
-		Onto.CreatNewExcelIndiv("Category",Category);
+		Onto.CreatNewExcelIndiv("Category", Category);
 		Onto.CreatNewExcelIndiv("Manager", Manager);
 		Onto.CreatNewExcelIndiv("Company", Company);
-		
-		Onto.OntoExcellFile(FileName);	
-		
-	}
-	
-	public static void main (String[] args)
-	{
-	ExcelToOnto Testfile= new ExcelToOnto(args[0]);
-	Testfile.ExcelOntoConv( "http://www.ontologie.fr/monOntologie#", args[0]+".owl");
+
+		Onto.OntoExcellFile(FileName);
 	}
 
+	public static void main(String[] args)
+	{
+		ExcelToOnto Testfile = new ExcelToOnto(args[0]);
+		Testfile.ExcelOntoConv("http://www.ontologie.fr/monOntologie#",
+		                       args[0] + ".owl");
+	}
 }
--- src/java/src/OntoExcel.java
+++ src/java/src/OntoExcel.java
@@ -11,52 +11,62 @@ import org.apache.jena.ontology.OntClass;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.rdf.model.ModelFactory;
 
-public class OntoExcel {
+public class OntoExcel
+{
 	OntModel ontologie;
 	String namespace;
-	
-	public OntoExcel (String NameSpace)
-	{ontologie = ModelFactory.createOntologyModel();	
-	namespace = NameSpace;
-	ontologie.createOntology (namespace);}
-	
-	
-	
-	public void CreateNewExceClass(String ClassName, String Prop,String Dom, String Ran)
-	{ ontologie.createClass (namespace + ClassName);
-	ObjectProperty newProp = ontologie.createObjectProperty (namespace + Prop);	
-	OntClass Domain = ontologie.getOntClass(namespace + Dom);
-	// set Domain and Rang
-	newProp.setDomain (Domain);
-	if (Ran!="no")
-			{OntClass Rang = ontologie.getOntClass(namespace + Ran);
+
+	public OntoExcel(String NameSpace)
+	{
+		ontologie = ModelFactory.createOntologyModel();
+		namespace = NameSpace;
+		ontologie.createOntology(namespace);
+	}
+
+	public void CreateNewExceClass(String ClassName, String Prop, String Dom,
+	                               String Ran)
+	{
+		ontologie.createClass(namespace + ClassName);
+		ObjectProperty newProp
+		    = ontologie.createObjectProperty(namespace + Prop);
+		OntClass Domain = ontologie.getOntClass(namespace + Dom);
+		// set Domain and Rang
+		newProp.setDomain(Domain);
+		if(Ran != "no")
+		{
+			OntClass Rang = ontologie.getOntClass(namespace + Ran);
 			newProp.setRange(Rang);
-			} 
-	}
-	public void CreatNewExcelIndiv (String ClassName, String Indiv)
-	{   OntClass Cname = ontologie.getOntClass(namespace + ClassName);
-		ontologie.createIndividual(namespace+Indiv, Cname);}
-
-	public void OntoExcellFile(String FileName)	
-	{FileOutputStream fichierSortie = null;
-
-	try {
-		fichierSortie = new FileOutputStream (new File (FileName));
-	}
-	catch (FileNotFoundException ex) {
-		Logger.getLogger (Main.class.getName ()).log (Level.SEVERE, null, ex);
+		}
 	}
 
-	ontologie.write (fichierSortie);
-try {
-fichierSortie.close();
-} catch (IOException e) {
-e.printStackTrace();
-}
-
+	public void CreatNewExcelIndiv(String ClassName, String Indiv)
+	{
+		OntClass Cname = ontologie.getOntClass(namespace + ClassName);
+		ontologie.createIndividual(namespace + Indiv, Cname);
 	}
-	public static void main(String[] args) {
+
+	public void OntoExcellFile(String FileName)
+	{
+		FileOutputStream fichierSortie = null;
+
+		try
+		{
+			fichierSortie = new FileOutputStream(new File(FileName));
+		}
+		catch(FileNotFoundException ex)
+		{
+			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+		}
+
+		ontologie.write(fichierSortie);
+		try
+		{
+			fichierSortie.close();
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
 	
-	
-}
+	}
 }
