9572a816b1cef04a477cedcfde86a0acfdbbf847
dducry <ducry_d@hotmail.fr>
2017-05-15 17:05:25 +0200

    Add form for DB

--- src/index.php
+++ src/index.php
@@ -8,5 +8,20 @@
     <input type="submit" value="Reference This Resource" name="submit">
 </form>
 
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Enter login and password:<br/>
+    <label for"url"> url : </label>
+    <input type="text" name="url" id="url"/><br/>
+
+    <label for"user"> userName : </label>
+    <input type="text" name="user" id="user"/><br/>
+
+    <label for"password"> password : </label>
+    <input type="password" value="password" name="password"/><br/>
+
+    <input type="submit" value="Reference This Resource" name="submit">
+</form>
+
+
 </body>
 </html>
