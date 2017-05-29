dc886323875be0aaac5d1e26935690106d0df328
Florian Cabot <florian.cabot@etu.unige.ch>
2017-05-29 23:33:46 +0200

    Added HTML to original form

--- src/index.php
+++ src/index.php
@@ -3,48 +3,63 @@
 <body onload="showExcel()">
 
 <script type="text/javascript">
-	function showExcel()
-	{
-		document.getElementById("excel_div").style.display = 'block';
+function showExcel()
+{
+	document.getElementById("excel_div").style.display = 'block';
 
-		document.getElementById("db_div").style.display = 'none';
-	}
-	function showDB()
-	{
-		document.getElementById("db_div").style.display = 'block';
+	document.getElementById("db_div").style.display = 'none';
+	document.getElementById("html_div").style.display = 'none';
+}
+function showDB()
+{
+	document.getElementById("db_div").style.display = 'block';
 
-		document.getElementById("excel_div").style.display = 'none';
-	}
+	document.getElementById("excel_div").style.display = 'none';
+	document.getElementById("html_div").style.display = 'none';
+}
+function showHTML()
+{
+	document.getElementById("html_div").style.display = 'block';
+
+	document.getElementById("excel_div").style.display = 'none';
+	document.getElementById("db_div").style.display = 'none';
+}
 </script>
 
 <form action="upload.php" method="post" enctype="multipart/form-data">
-    <input type="radio" name="resource-type" value="excel" onclick="showExcel()" checked> Excel
-    <input type="radio" name="resource-type" value="db" onclick="showDB()"> DataBase
-    <input type="radio" name="resource-type" value="html"> HTML
-    <input type="radio" name="resource-type" value="service"> Service
+	<input type="radio" name="resource-type" value="excel" onclick="showExcel()" checked> Excel
+	<input type="radio" name="resource-type" value="db" onclick="showDB()"> DataBase
+	<input type="radio" name="resource-type" value="html" onclick="showHTML()"> HTML
+	<input type="radio" name="resource-type" value="service"> Service
 
 <!-- Excel -->
-    <div id="excel_div">
-    Select resource to reference:<br/>
-    <input type="file" name="fileToUpload" id="fileToUpload"></br/>
-    </div>
+	<div id="excel_div">
+	Select resource to reference:<br/>
+	<input type="file" name="fileToUpload" id="fileToUpload"></br/>
+	</div>
 
 
 <!-- DB -->
-    <div id="db_div">
-    Enter login and password:<br/>
-    <label for"url" id="url_label"> url : </label>
-    <input type="text" name="url" id="url"/><br/>
+	<div id="db_div">
+	Enter login and password:<br/>
+	<label for"url" id="url_label"> url : </label>
+	<input type="text" name="url" id="url"/><br/>
 
-    <label for"user" id="user_label"> userName : </label>
-    <input type="text" name="user" id="user"/><br/>
+	<label for"user" id="user_label"> userName : </label>
+	<input type="text" name="user" id="user"/><br/>
 
-    <label for"password" id="password_label"> password : </label>
-    <input type="password" name="password" id="password"/><br/>
-    </div>
+	<label for"password" id="password_label"> password : </label>
+	<input type="password" name="password" id="password"/><br/>
+	</div>
+
+<!-- HTML -->
+	<div id="html_div">
+	Select HTML resource to reference:<br/>
+	<input type="file" name="HTMLfileToUpload" id="HTMLfileToUpload"></br/>
+	</div>
 
 <!-- Common -->
-    <input type="submit" value="Reference This Resource" name="submit">
+	<input type="submit" value="Reference This Resource" name="submit">
 
 </form>
 
