54e9f07cf5546db5e76af12d0846228bc24e4741
Florian Cabot <florian.cabot@etu.unige.ch>
2017-05-30 00:01:31 +0200

    Fixed PHP scripts for Excel

--- src/index.php
+++ src/index.php
@@ -9,6 +9,7 @@ function showExcel()
 
 	document.getElementById("db_div").style.display = 'none';
 	document.getElementById("html_div").style.display = 'none';
+	document.getElementById("service_div").style.display = 'none';
 }
 function showDB()
 {
@@ -16,6 +17,7 @@ function showDB()
 
 	document.getElementById("excel_div").style.display = 'none';
 	document.getElementById("html_div").style.display = 'none';
+	document.getElementById("service_div").style.display = 'none';
 }
 function showHTML()
 {
@@ -23,24 +25,38 @@ function showHTML()
 
 	document.getElementById("excel_div").style.display = 'none';
 	document.getElementById("db_div").style.display = 'none';
+	document.getElementById("service_div").style.display = 'none';
+}
+function showService()
+{
+	document.getElementById("service_div").style.display = 'block';
+
+	document.getElementById("excel_div").style.display = 'none';
+	document.getElementById("db_div").style.display = 'none';
+	document.getElementById("html_div").style.display = 'none';
 }
 </script>
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
-	<input type="radio" name="resource-type" value="excel" onclick="showExcel()" checked> Excel
-	<input type="radio" name="resource-type" value="db" onclick="showDB()"> DataBase
-	<input type="radio" name="resource-type" value="html" onclick="showHTML()"> HTML
-	<input type="radio" name="resource-type" value="service"> Service
+<input type="radio" name="resource-type" value="excel" onclick="showExcel()" checked> Excel
+<input type="radio" name="resource-type" value="db" onclick="showDB()"> DataBase
+<input type="radio" name="resource-type" value="html" onclick="showHTML()"> HTML
+<input type="radio" name="resource-type" value="service" onclick="showService()"> Service
 
 <!-- Excel -->
 	<div id="excel_div">
+	<form action="uploadExcel.php" method="post" enctype="multipart/form-data">
 	Select resource to reference:<br/>
 	<input type="file" name="fileToUpload" id="fileToUpload"></br/>
+
+	<input type="submit" value="Reference This Resource" name="submit">
+
+	</form>
 	</div>
 
 
 <!-- DB -->
 	<div id="db_div">
+	<form action="uploadDB.php" method="post" enctype="multipart/form-data">
 	Enter login and password:<br/>
 	<label for"url" id="url_label"> url : </label>
 	<input type="text" name="url" id="url"/><br/>
@@ -50,18 +66,24 @@ function showHTML()
 
 	<label for"password" id="password_label"> password : </label>
 	<input type="password" name="password" id="password"/><br/>
+	<input type="submit" value="Reference This Resource" name="submit">
+
+	</form>
 	</div>
 
 <!-- HTML -->
 	<div id="html_div">
+	<form action="uploadHTML.php" method="post" enctype="multipart/form-data">
 	Select HTML resource to reference:<br/>
-	<input type="file" name="HTMLfileToUpload" id="HTMLfileToUpload"></br/>
-	</div>
-
-<!-- Common -->
+	<input type="file" name="fileToUpload" id="fileToUpload"></br/>
 	<input type="submit" value="Reference This Resource" name="submit">
 
-</form>
+	</form>
+	</div>
+
+<!-- Service -->
+	<div id="service_div">
+	</div>
 
 
 </body>
---
+++ src/uploadExcel.php
@@ -0,0 +1,37 @@
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+// Check if file already exists
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+if (file_exists($target_file)) {
+	echo "Sorry, resource already exists.";
+	$uploadOk = 0;
+} 
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+	echo "Sorry, your resource is too large to upload.";
+	$uploadOk = 0;
+} 
+//Load JPG, JPEG, PNG, and GIF files. All other file types gives an error message before setting $uploadOk to 0:
+// Allow certain file formats
+if($imageFileType != "xls" && $imageFileType != "xlsx")//&& $imageFileType != "png" && $imageFileType != "jpeg"
+{
+	echo "Sorry, only XLS files are allowed.";
+	$uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+	echo "Sorry, your resource was not uploaded.";
+	// if everything is ok, try to upload file
+} else {
+	if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+		echo "The resource ". basename( $_FILES["fileToUpload"]["name"]). " has been referenced.";
+		echo '<form action="index.php"><input type="submit" value="Go Back" /></form>';
+		exec('java -cp "java/bin/:java/lib/*" rfp.ResourceToOnto --excel "uploads/' . basename( $_FILES["fileToUpload"]["name"]) . '" > java-debug.log');
+		unlink($target_file);
+	} else {
+		echo "Sorry, there was an error uploading your resource.";
+	}
+}
+?>
